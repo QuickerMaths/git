bb81e1e942ac628e20bf847fb60c24c57c5576fa
QuickerMaths <mateusz.szalowicz@gmail.com>
2024-04-23 00:01:56 +0200

    inital setup for git init command

--- src/cli.ts
+++ src/cli.ts
@@ -1,11 +1,20 @@
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers'
+import { init } from './init.ts';
 
 export function cli(args: string[]) {
     return yargs(hideBin(args))
-    .command('curl <url>', 'fetch the contents of the URL', () => {}, (argv) => {
-        console.info(argv)
+    .command('init [path]', 'initalizes empty git repository',
+    (argv) => {
+        return argv.positional('path', {
+            describe: 'path to initialize git repository, defaults to current directory',
+            default: '.'
+        })
+    }, 
+    async (argv) => {
+        await init(argv.path)
     })
     .demandCommand(1)
 }
+
 
---
+++ src/init.ts
@@ -0,0 +1,45 @@
+import fs from 'fs/promises';
+import path from 'node:path';
+
+let isReinitialized = false;
+
+async function ensureExists(dirPath: string, callback: () => Promise<void>) {
+    try {
+        await fs.access(dirPath);
+    } catch {
+        await callback()
+    }
+}
+
+async function createGitDir(gitDirPath: string) {
+    try {
+        await fs.mkdir(gitDirPath, { recursive: true });
+    } catch(err) {
+        console.log('Unanble to create .git directory', err);
+        process.exit(1);
+    }
+}
+
+async function writeHeadFile(gitDirPath: string) {
+    const filePath = path.join(gitDirPath, 'HEAD');
+    const writeHeadFile = () => fs.writeFile(filePath, 'ref: refs/heads/master\n');
+
+    ensureExists(filePath, writeHeadFile);
+}
+
+function sendSuccessMessage(gitDirPath: string, isReinitialized: boolean) {
+    console.log(`${isReinitialized ? 'Reinitialized existing' : 'Initialized empty'} git repository in`, gitDirPath);
+}
+
+export async function init(argvPath: string){
+    const gitDirPath = path.resolve(process.cwd(), argvPath, '.git');
+
+    try {
+        await fs.access(gitDirPath);
+        isReinitialized = true;
+    } catch {
+        await createGitDir(gitDirPath);
+    }
+    await writeHeadFile(gitDirPath);
+    sendSuccessMessage(gitDirPath, isReinitialized);
+}
