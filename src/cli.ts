9825d438613bb9e869d24a08eb0d182d203c3a52
QuickerMaths <mateusz.szalowicz@gmail.com>
2024-04-23 16:22:20 +0200

    rafctor cli file and add type option to hash-object command

--- src/cli.ts
+++ src/cli.ts
@@ -5,33 +5,49 @@ import { hashObject } from './hash-object.ts';
 
 export function cli(args: string[]) {
     return yargs(hideBin(args))
-    .command('init [path]', 'initalizes empty git repository',
-    (argv) => {
-        return argv.positional('path', {
-            describe: 'path to initialize git repository, defaults to current directory',
-            default: '.'
-        })
-    }, 
-    async (argv) => await init(argv.path, !!argv.quiet))
-    .option('quiet', {
-        alias: 'q',
-        type: 'boolean',
-        description: 'Print only error messages.',
-        default: false
-    })
     .command(
-        'hash-object', 
-        'computes the object ID value for an object with specified type with contents of the named file, and optionally writes the resulting object into database', 
-        (_argv) => {},
-        async (_argv) => await hashObject()
+        'init [path]', 
+        'initalizes empty git repository',
+        (argv) => {
+            return argv
+            .positional('path', {
+                describe: 'path to initialize git repository, defaults to current directory',
+                default: '.'
+            })
+            .option('quiet', {
+                alias: 'q',
+                type: 'boolean',
+                description: 'Print only error messages.',
+                default: false
+            })
+        }, 
+        async (argv) => await init(argv.path, !!argv.quiet)
     )
-    .option('write', {
-        alias: 'w',
-        type: 'boolean',
-        description: 'Actually write the object into the database',
-        default: false
-    })
-    .demandCommand(1)
+    .command(
+        'hash-object [file]', 
+        'computes the object ID value for an object with specified type with contents of the named file, and optionally writes the resulting object into database', 
+            (argv) => {
+                return argv
+                .positional('file', {
+                    describe: 'file to hash',
+                    type: 'string',
+                    default: ''
+                })
+                .option('type', {
+                    alias: 't',
+                    type: 'string',
+                    description: 'specifies type (blob | commit | tree | tag). Defaults to blob.',
+                    default: 'blob'
+                })
+                .option('write', {
+                    alias: 'w',
+                    type: 'boolean',
+                    description: 'actually write the object into the database',
+                    default: false
+                })
+            },
+            async (argv) => await hashObject(argv.file, argv.type)
+    )
 }
 
 
