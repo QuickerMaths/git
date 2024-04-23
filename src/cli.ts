f980e261b77e0851fbcc1ba6897ba325e980d84f
QuickerMaths <mateusz.szalowicz@gmail.com>
2024-04-23 23:56:56 +0200

    added ensureGitRepo

--- src/cli.ts
+++ src/cli.ts
@@ -2,11 +2,46 @@ import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers'
 import { init } from './init.ts';
 import { hashObject } from './hash-object.ts';
+import path from 'node:path'
+import fs from 'fs/promises'
+
+async function exists(path: string) {
+    try {
+        await fs.access(path)
+        return true
+    } catch {
+        return false
+    }
+}
+
+async function ensureGitRepo(): Promise<string> {
+  let root = process.cwd();
+  let pathToGit: string;
+
+  while (root !== '/') {
+    pathToGit = path.join(root, '.git');
+
+    if (await exists(pathToGit)) {
+      return root;
+    }
+    root = path.dirname(root);
+  }
+
+  pathToGit = path.join(root, '.git');
+  if (await exists(pathToGit)) {
+    return root;
+  }
+
+  process.stderr.write(
+    'fatal: not a git repository (or any of the parent directories): .git\r\n'
+  );
+  process.exit(1);
+}
 
 async function commandWrapper(callback: () => Promise<string | undefined>) {
     try {
         const output = await callback();
-        if(output) process.stdout.write(output);
+        if(output) process.stdout.write(output + '\r\n');
         process.exit(0);
     } catch(error) {
         const e = error as Error;
@@ -58,6 +93,9 @@ export function cli(args: string[]) {
                     default: false
                 })
             },
-            async (argv) => await commandWrapper(() => hashObject(argv.file, argv.type))
+            async (argv) => {
+                const gitRoot = await ensureGitRepo()
+                await commandWrapper(() => hashObject(gitRoot, argv.file, argv.type, argv.write))
+            }
     )
 }
