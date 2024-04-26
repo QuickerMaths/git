67567d2352370f495186f493ad23adb00d4b16b3
QuickerMaths <mateusz.szalowicz@gmail.com>
2024-04-26 14:41:55 +0200

    remove ensureGitRepo from cli.ts

--- src/cli.ts
+++ src/cli.ts
@@ -1,42 +1,8 @@
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
-import path from 'node:path';
-import fs from 'fs/promises';
 import { hashObject } from './commands/hash-object';
 import { init } from './commands/init';
-
-async function exists(path: string) {
-    try {
-        await fs.access(path)
-        return true
-    } catch {
-        return false
-    }
-}
-
-async function ensureGitRepo(): Promise<string> {
-  let root = process.cwd();
-  let pathToGit: string;
-
-  while (root !== '/') {
-    pathToGit = path.join(root, '.git');
-
-    if (await exists(pathToGit)) {
-      return root;
-    }
-    root = path.dirname(root);
-  }
-
-  pathToGit = path.join(root, '.git');
-  if (await exists(pathToGit)) {
-    return root;
-  }
-
-  process.stderr.write(
-    'fatal: not a git repository (or any of the parent directories): .git\r\n'
-  );
-  process.exit(1);
-}
+import { ensureGitRepo } from './utils/ensureGitRepo';
 
 async function commandWrapper(callback: () => Promise<string | string[] | undefined | void>) {
     try {
