68bacedd330266e37e3c5a86e2d20c1dcfef9f61
QuickerMaths <mateusz.szalowicz@gmail.com>
2024-04-28 00:07:27 +0200

    added offset constants for encoding index file

---
+++ src/constants/constants.ts
@@ -0,0 +1,14 @@
+export const PREFIX_SIZE = 62;
+export const CTIME_OFFSET = 0;
+export const CTIME_NANO_OFFSET = 4;
+export const MTIME_OFFSET = 8;
+export const MTIME_NANO_OFFSET = 12;
+export const DEV_OFFSET = 16;
+export const INO_OFFSET = 20;
+export const MODE_OFFSET = 24;
+export const UID_OFFSET = 28;
+export const GID_OFFSET = 32;
+export const FILES_SIZE_OFFSET = 36;
+export const SHA_OFFSET = 40;
+export const FLAGS_OFFSET = 60;
+export const NAME_OFFSET = 62;
