143cf370f34959d5b6a8c6e2d54c902f95538a63
QuickerMaths <mateusz.szalowicz@gmail.com>
2024-04-29 12:21:30 +0200

    added parseIndex function and implemented it in update-index

--- src/commands/update-index.ts
+++ src/commands/update-index.ts
@@ -1,22 +1,39 @@
+import path from 'path';
 import fs from 'fs/promises';
 import { GitIndex } from '../objects/git-index';
 import { IGitEntry } from '../types/types';
 import { createIndexEntry } from '../utils/createIndexEntry';
+import { parseIndex } from '../utils/parseIndex';
+import { exists } from '../utils/exists';
 
 export async function updateIndex(gitRoot: string, files: string [], add: boolean) {
     const entries: IGitEntry[] = [];
+    const pathToIndex = path.resolve(gitRoot, '.git/index');
 
-    for(const file of files) {
+    const sortedFiles = files.sort();
+
+    for(const file of sortedFiles) {
         try {
             await fs.access(file);
             const entry = await createIndexEntry(file, gitRoot);
             entries.push(entry);
-        } catch {
+        } catch(err: any) {
+            if(err.code === 'EISDIR') {
+                throw Error(`error: ${file}: is a directory - add file inside instead`)
+            }
+
             throw Error(`fatal: Cannot open '${file}': No such file or directory.`);
         }
     }
 
-    const index = new GitIndex({ signature: 'DIRC', version: 2 }, entries);
+    let index: GitIndex;
 
+    if(await exists(pathToIndex)) {
+        index = await parseIndex(pathToIndex);
+        console.log(index);
+    } else {
+        index = new GitIndex({ signature: 'DIRC', version: 2 }, entries)
+    }
+    
     if(add && !!files.length) await index.write();
 }
---
+++ src/utils/parseIndex.ts
@@ -0,0 +1,71 @@
+import fs from 'fs/promises';
+import { GitIndex } from '../objects/git-index';
+import { IGitEntry } from '../types/types';
+import { CTIME_OFFSET, CTIME_NANO_OFFSET, MTIME_OFFSET, MTIME_NANO_OFFSET, DEV_OFFSET, INO_OFFSET, MODE_OFFSET, UID_OFFSET, GID_OFFSET, FILES_SIZE_OFFSET, FLAGS_OFFSET, NAME_OFFSET, SHA_OFFSET } from '../constants/constants';
+
+function parsedEntry(content: Buffer, entryPosition: number) {
+    const parsedEntry = {} as IGitEntry;
+
+    parsedEntry.ctimeSec = content.readInt32BE(entryPosition + CTIME_OFFSET);
+    parsedEntry.ctimeNano = content.readInt32BE(entryPosition + CTIME_NANO_OFFSET);
+
+    parsedEntry.mtimeSec = content.readInt32BE(entryPosition + MTIME_OFFSET);
+    parsedEntry.mtimeNano = content.readInt32BE(entryPosition + MTIME_NANO_OFFSET);
+
+    parsedEntry.dev = content.readInt32BE(entryPosition + DEV_OFFSET);
+
+    parsedEntry.ino = content.readInt32BE(entryPosition + INO_OFFSET);
+
+    parsedEntry.modeType = content.readInt32BE(entryPosition + MODE_OFFSET);
+
+    parsedEntry.uid = content.readInt32BE(entryPosition + UID_OFFSET);
+
+    parsedEntry.gid = content.readInt32BE(entryPosition + GID_OFFSET);
+
+    parsedEntry.fileSize = content.readInt32BE(entryPosition + FILES_SIZE_OFFSET);
+
+    parsedEntry.sha = content
+    .subarray(entryPosition + SHA_OFFSET, entryPosition + SHA_OFFSET + 20)
+    .toString('hex');
+
+    const flags = content.readInt16BE(entryPosition + FLAGS_OFFSET);
+    parsedEntry.stage = (flags & (0b11 << 12)) >> 12;
+    
+    parsedEntry.assumeValid = (flags & (0b11 << 12)) >> 12; 
+
+    const nameLength = 0xfff & flags;
+    parsedEntry.name = content
+    .subarray(entryPosition + NAME_OFFSET, entryPosition + NAME_OFFSET + nameLength)
+    .toString('utf8');
+
+
+    return { nameLength, parsedEntry };
+} 
+
+export async function parseIndex(pathToIndex: string) { 
+    const indexFile = await fs.readFile(pathToIndex);
+    let entryPosition = 0;
+
+    const signature = indexFile.subarray(0, 4).toString();
+    if(signature !== 'DIRC') throw Error('fatal: Invalid index signature'); 
+
+    const version = indexFile.readInt32BE(4);
+    if(version !== 2) throw Error('fatal: Invalid index version: ccgit only supports version 2');
+
+    const entriesCount = indexFile.readInt32BE(8);
+    const entries = new Array<IGitEntry>(entriesCount);
+    const content = indexFile.subarray(12);
+
+    for(let i = 0; i < entriesCount; i++) {
+        const { nameLength, parsedEntry: entry } = parsedEntry(content, entryPosition);        
+        entryPosition += NAME_OFFSET + nameLength;
+
+        while (content[entryPosition] === Buffer.from('\0')[0] && entryPosition < content.length) {
+            entryPosition++;
+        }
+
+        entries[i] = entry;
+    }
+
+    return new GitIndex({ signature, version }, entries);
+}
