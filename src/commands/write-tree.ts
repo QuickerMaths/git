b86751ad9252d06dcff600fe0787c9cc0bffa785
QuickerMaths <mateusz.szalowicz@gmail.com>
2024-05-02 21:26:39 +0200

    implement writing and saveing the tree

--- src/commands/write-tree.ts
+++ src/commands/write-tree.ts
@@ -1,3 +1,23 @@
-export async function writeTree(gitRoot: string) {
-    console.log('write-tree');
+import path from "path";
+import { parseIndex } from "../utils/parseIndex";
+import { GitIndex } from "../objects/git-index";
+import { exists } from "../utils/exists";
+import { Tree } from "../objects/tree";
+
+export async function writeTree(gitRoot: string, write: boolean = true) {
+    let index: GitIndex;
+    const pathToIndex = path.resolve(gitRoot, '.git/index');
+
+    if(await exists(pathToIndex)) {
+        index = await parseIndex(pathToIndex);
+    } else {
+        index = new GitIndex({ signature: 'DIRC', version: 2 }, []);
+    }
+    
+    const tree = new Tree();
+    tree.createTree(index.entries);
+
+    const treeHash = tree.treeRoot.hashTree(gitRoot, write);
+
+    return treeHash;
 }
