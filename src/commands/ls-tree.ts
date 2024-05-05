e43b30265ca5572b0364c50ef4cb5efc5c1b25f5
QuickerMaths <mateusz.szalowicz@gmail.com>
2024-05-05 11:55:10 +0200

    implemented functionality for ls-tree

--- src/commands/ls-tree.ts
+++ src/commands/ls-tree.ts
@@ -1,3 +1,14 @@
-export function lsTree(gitRoot: string, argvHash: string, argvRecursive: boolean, argvShowTree: boolean) {
-    console.log('lsTree');
+import { FileMode } from "../enums/enums";
+import { Tree, TreeObject } from "../objects/tree";
+import { prepareTreeOutput, processTree } from "../utils/processTree";
+
+export async function lsTree(gitRoot: string, argvHash: string, argvRecursive: boolean, argvShowTree: boolean) {
+    const tree = new Tree();
+    const treeRoot = new TreeObject(FileMode.DIR, '', '', argvHash);
+    tree.treeRoot = treeRoot;
+
+    const treeArray = [treeRoot];
+    await processTree(gitRoot, tree, treeArray, argvRecursive);
+
+    return prepareTreeOutput(tree, argvRecursive, argvShowTree); 
 }
