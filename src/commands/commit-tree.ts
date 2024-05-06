269163ed91e063e5e3d9fef896778f2bd2fb1b3a
QuickerMaths <mateusz.szalowicz@gmail.com>
2024-05-06 14:34:52 +0200

    implemented commit-tree functionality

--- src/commands/commit-tree.ts
+++ src/commands/commit-tree.ts
@@ -1,3 +1,9 @@
-export async function commitTree(gitRoot: string, argvTree: string) {
-    console.log('commitTree');
+import { Commit } from "../objects/commit";
+
+export async function commitTree(gitRoot: string, argvTree: string, argvMessage: string) {
+    const commit = new Commit();
+
+    const hash = await commit.createCommit(gitRoot, argvTree, argvMessage);
+
+    return hash;
 }
