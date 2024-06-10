341c32e7100391c6026f8bf2c7f3a9c1071d19d3
QuickerMaths <mateusz.szalowicz@gmail.com>
2024-06-10 16:58:44 +0200

    refactored writeTree function to be sync

--- src/commands/write-tree.ts
+++ src/commands/write-tree.ts
@@ -1,18 +1,18 @@
 import path from "path";
+import fs from 'fs';
 import { parseIndex } from "../utils/parseIndex";
 import { GitIndex } from "../objects/git-index";
-import { exists } from "../utils/exists";
 import { Tree } from "../objects/tree";
 
 export async function writeTree(gitRoot: string, write: boolean = true) {
     let index: GitIndex;
     const pathToIndex = path.resolve(gitRoot, '.git/index');
 
-    if(await exists(pathToIndex)) {
+    if(fs.existsSync(pathToIndex)) {
         index = parseIndex(pathToIndex);
     } else {
         index = new GitIndex({ signature: 'DIRC', version: 2 }, []);
-        await index.write(gitRoot);
+        index.write(gitRoot);
     }
     
     const tree = new Tree();
